e4680190c424af5979a689dd89abb91979c79295
Daniel Aragón <daniel.aragon.0235@hotmail.com>
2018-10-08 17:00:33 -0500

    commit pre unit test

---
+++ miluzsrl.Tests/Data_Access/CategoriaRepositoryTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace miluzsrl.Tests.Data_Access
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
